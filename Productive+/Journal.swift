958b16c4f6e321769c40c0b1295e955991489151
Sachi Mehta <sachimehta16@gmail.com>
2023-08-17 18:09:11 -0400

    journal added

--- Productive+/Journal.swift
+++ Productive+/Journal.swift
@@ -1,20 +1,32 @@
-//
-//  Journal.swift
-//  Productive+
-//
-//  Created by scholar on 8/16/23.
-//
 
 import SwiftUI
-
 struct Journal: View {
-    var body: some View {
-        Text("Journal Page Wooooo!")
+  @State private var journalEntry = ""
+  var body: some View {
+     ZStack {
+         Color(red: 0.959, green: 0.924, blue: 0.925)
+                   .ignoresSafeArea()
+         VStack{
+         Image("journalTemplate")
+           .renderingMode (.original)
+       Spacer ()
+         Button (action:{print("clear")
+             journalEntry = ""
+         }, label: {Image("newEntry").resizable()
+            .aspectRatio(contentMode: .fit)
+            .frame(height: 100.0)})
+      }
+       TextField("Type out your feelings", text: $journalEntry, axis: .vertical)
+         .padding(.all, 20.0)
+         .frame(width: 250.0, height: 300.0)
+         .border(Color("popYellow"), width: 2)
+         .multilineTextAlignment(.center)
+         .foregroundColor(.accentColor)
+     }
+      }
     }
-}
-
 struct Journal_Previews: PreviewProvider {
-    static var previews: some View {
-        Journal()
-    }
+  static var previews: some View {
+    Journal()
+  }
 }
